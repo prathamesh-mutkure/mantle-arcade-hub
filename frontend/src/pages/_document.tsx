6db9300526a0a0dc8318e8c41af31f5a2bacaa8c
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-12-01 11:12:00 +0530

    Added Google Analytics

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function Document() {
   return (
@@ -7,6 +8,8 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+
+        <GoogleAnalytics gaId="G-9SSE6ELV91" />
       </body>
     </Html>
   );
