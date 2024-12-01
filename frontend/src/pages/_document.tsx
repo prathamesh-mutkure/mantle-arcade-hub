857d5a4932f2d2618a9f3ccb765d0dd6512a256d
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-12-01 13:32:10 +0530

    Manual GTag

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,10 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          async={true}
+          src="https://www.googletagmanager.com/gtag/js?id=G-9SSE6ELV91"
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9SSE6ELV91');
+            `,
+          }}
+        />
+      </Head>
       <GoogleAnalytics gaId="G-9SSE6ELV91" />
 
       <body className="antialiased">
